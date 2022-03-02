92c1bbe21747c3dd239cfe3175a0ae5df0541692
F1NH4WK <araujoplayer@gmail.com>
2022-03-02 15:00:36 -0300

    y

--- CalendarToSheet.js
+++ CalendarToSheet.js
@@ -1,33 +1,35 @@
 function pullDataToSheet() {
 
-    function addNextRow(lastRow, i){
-      val = sheet.getRange(lastRow, 1)
-      val.setValue(day[i].getTitle());
-    }
-  
-    function addNextCollumn(lastRow, i){
-      val = sheet.getRange(lastRow, 2)
-      val.setValue(day[i].getStartTime());
-      val = sheet.getRange(lastRow, 3);
-      val.setValue(day[i].getEndTime());
-    }
-  
-    // SETTING VARIABLES TO SHEET
-    let sheet = SpreadsheetApp.getActiveSheet();
-    let lastRow = sheet.getLastRow() + 1;
-    let val = sheet.getRange(lastRow, 1);
-  
-    // SETTING VARIABLES TO CALENDAR
-    let helper = new Date();
-    let now = new Date(helper.getTime() + (2*60*60*1000));
-    let hoursNext = new Date(now.getTime() + (2*60*60*1000))
-    let userCalendar = CalendarApp.getDefaultCalendar();
-    let day = userCalendar.getEvents(now, hoursNext);
-  
-    // ADDING VALUES TO SHEET
-    for (i = 0; i <= day.length-1; i++){
-      addNextRow(lastRow, i)
-      addNextCollumn(lastRow, i)
-      lastRow++;
-    };
+  function addNextRow(lastRow, i){
+    val = sheet.getRange(lastRow, 1)
+    val.setValue(day[i].getTitle());
+    console.log(day[i].getTitle())
+  }
+
+  function addNextCollumn(lastRow, i){
+    val = sheet.getRange(lastRow, 2)
+    val.setValue(day[i].getStartTime());
+    val = sheet.getRange(lastRow, 3);
+    val.setValue(day[i].getEndTime());
+  }
+
+  // SETTING VARIABLES TO SHEET
+  const sheet = SpreadsheetApp.getActiveSheet();
+  let lastRow = sheet.getLastRow() + 1;
+  let val = sheet.getRange(lastRow, 1);
+
+  // SETTING VARIABLES TO CALENDAR
+  const helper = new Date();
+  const yourChoice = 2 // IN HOURS - IF U WANT TO GET THE EVENTS THAT ARE OCURRING IN NEXT 2 HOURS.
+  const now = new Date(helper.getTime() + 1200*1000* 2);
+  const target = new Date(now.getTime() + 1200*1000 * yourChoice)
+  const userCalendar = CalendarApp.getDefaultCalendar();
+  const day = userCalendar.getEvents(now, target);
+
+  // ADDING VALUES TO SHEET
+  for (i = 0; i <= day.length-1; i++){
+    addNextRow(lastRow, i)
+    addNextCollumn(lastRow, i)
+    lastRow++;
+  };
 }
