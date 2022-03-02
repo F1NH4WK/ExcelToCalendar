be59f5ac1dea9687ec29ce798a126280ec75947c
F1NH4WK <araujoplayer@gmail.com>
2022-03-02 01:55:50 -0300

    starting some

--- CalendarToSheet.js
+++ CalendarToSheet.js
@@ -1,1 +1,33 @@
-a
+function pullDataToSheet() {
+
+    function addNextRow(lastRow, i){
+      val = sheet.getRange(lastRow, 1)
+      val.setValue(day[i].getTitle());
+    }
+  
+    function addNextCollumn(lastRow, i){
+      val = sheet.getRange(lastRow, 2)
+      val.setValue(day[i].getStartTime());
+      val = sheet.getRange(lastRow, 3);
+      val.setValue(day[i].getEndTime());
+    }
+  
+    // SETTING VARIABLES TO SHEET
+    let sheet = SpreadsheetApp.getActiveSheet();
+    let lastRow = sheet.getLastRow() + 1;
+    let val = sheet.getRange(lastRow, 1);
+  
+    // SETTING VARIABLES TO CALENDAR
+    let helper = new Date();
+    let now = new Date(helper.getTime() + (2*60*60*1000));
+    let hoursNext = new Date(now.getTime() + (2*60*60*1000))
+    let userCalendar = CalendarApp.getDefaultCalendar();
+    let day = userCalendar.getEvents(now, hoursNext);
+  
+    // ADDING VALUES TO SHEET
+    for (i = 0; i <= day.length-1; i++){
+      addNextRow(lastRow, i)
+      addNextCollumn(lastRow, i)
+      lastRow++;
+    };
+}
